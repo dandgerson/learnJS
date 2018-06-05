3e1969ba7cc98b9441f0a3ff99374d064a196e3c
dandgerson <dandgerson@gmail.com>
2018-06-05 15:13:29 +0300

    dragElem: positioning elems

--- iliakan/2017-06-29/drag-heroes/dragElem.js
+++ iliakan/2017-06-29/drag-heroes/dragElem.js
@@ -10,7 +10,9 @@ class DragElem {
         let box = elem.getBoundingClientRect();
         return {
             top: box.top + pageYOffset,
-            left: box.left + pageXOffset
+            left: box.left + pageXOffset,
+            width: box.width,
+            height: box.height
         };
     }
 
@@ -29,29 +31,38 @@ class DragElem {
 
         this.container.append(this.dragElem);
 
-        this._moveAtX();
+        this._moveAt();
 
         this.dragElem.addEventListener('dragstart', event => event.preventDefault());
         document.onmousemove = event => this.onMouseMove();
         this.dragElem.onmouseup = event => this.onMouseUp();
     }
 
-    _moveAtX() {
-        let containerCoords = this.container.getBoundingClientRect();
-
+    _moveAt() {
+        let boxCoord = this.dragElem.getBoundingClientRect();
         let left = event.pageX - this._shiftX;
         let top = event.pageY - this._shiftY;
+        console.log(boxCoord);
+        console.log('left: ' + left);
+        console.log('top: ' + top);
+        console.log('scrollTop: ' + document.documentElement.scrollTop);
+        console.log('pageYOffset: ' + pageYOffset);
 
+        if (left < 0) left = 0;
+        if (left + this.dragElemCoords.width > document.documentElement.clientWidth)
+            left = document.documentElement.clientWidth - this.dragElemCoords.width;
+        if (top < pageYOffset) top = pageYOffset;
+        if (top + this.dragElemCoords.height > pageYOffset + document.documentElement.clientHeight)
+            top = pageYOffset + document.documentElement.clientHeight - this.dragElemCoords.height;
         this.dragElem.style.left = left + 'px';
         this.dragElem.style.top = top + 'px';
     }
 
-    onMouseMove(event) {
-        this._moveAtX();
+    onMouseMove() {
+        this._moveAt();
     }
 
     onMouseUp() {
-        console.log(event);
         document.onmousemove = null;
         document.onmouseup = null;
     }
